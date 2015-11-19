3e7b342f16122a97e28f521045a76e5d693dd8c3
k2works <kakimomokuri@gmail.com>
2015-11-19 16:26:57 +0900

    2-2 変数と定数

---
+++ 2-2/2-2-1.rb
@@ -0,0 +1,18 @@
+# ローカル変数
+foo = 'foo in toplevel'
+
+def display_foo
+  puts foo
+end
+
+puts foo
+#display_foo
+
+greeting = "Hello,"
+people = ['Alice','Bob']
+
+people.each do |person|
+  puts greeting + person
+end
+
+puts person
---
+++ 2-2/2-2-3.rb
@@ -0,0 +1,12 @@
+# 定数
+
+FOO_BAR = 'bar'
+
+puts FOO_BAR
+
+FOO_BAR = 'bar'
+
+FOO_BAR = 'foo'
+
+puts FOO_BAR
+
